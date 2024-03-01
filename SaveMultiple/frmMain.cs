0dfa5dd2cd6758b78190642d4904494c2d5c2f48
omc10079 <kida-seiji@orient-microwave.co.jp>
2024-03-01 11:43:40 +0900

    プロジェクト ファイルを追加します。

---
+++ SaveMultiple/frmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using AgilentPNA835x;
+
+namespace SaveMultiple
+{
+    public partial class frmMain : Form
+    {
+        private static IApplication app;
+        private static IScpiStringParser scpi;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Type typeFromProgID = Type.GetTypeFromProgID("AgilentPNA835x.Application");
+            app = (IApplication)Activator.CreateInstance(typeFromProgID);
+            scpi = app.ScpiStringParser;
+        }
+    }
+}
