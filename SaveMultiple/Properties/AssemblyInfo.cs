15aa2185210959bf58b3d72f8b283b9a8cf61f77
omc10079 <kida-seiji@orient-microwave.co.jp>
2024-03-02 13:58:17 +0900

    Ver,1.0.0.1

--- SaveMultiple/Properties/AssemblyInfo.cs
+++ SaveMultiple/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SaveMultiple")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Orient Microwave Corp.")]
+[assembly: AssemblyCompany("OrientMicrowave")]
 [assembly: AssemblyProduct("SaveMultiple")]
 [assembly: AssemblyCopyright("Copyright © Orient Microwave Corp.")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
